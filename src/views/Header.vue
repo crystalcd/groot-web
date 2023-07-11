4eb8e2630e4e03ef342c79342f88191cdfb5a7b3
byronc <byron.chen@zoom.us>
2023-07-11 17:42:28 +0800

    init

---
+++ src/views/Header.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    {{ count }}
+  </div>
+  <button @click="add">add</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    add() {
+      this.count++
+    }
+  },
+  mounted() {
+    this.add()
+  }
+}
+</script>
+<style lang=""></style>
